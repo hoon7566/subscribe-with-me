2bd404e631371aad337eaec0cbb1ee5ad636557a
hoon7566 <hoon7566@gmail.com>
2021-12-29 11:57:35 +0900

    git init

---
+++ src/main/java/me/hoon/subscribewithme/SubscribeWithMeApplication.java
@@ -0,0 +1,13 @@
+package me.hoon.subscribewithme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubscribeWithMeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SubscribeWithMeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/hoon/subscribewithme/SubscribeWithMeApplicationTests.java
@@ -0,0 +1,13 @@
+package me.hoon.subscribewithme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubscribeWithMeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
